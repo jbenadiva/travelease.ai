f3615e199a3db4cc98d071a08594e2a836e7c24a
jbenadiva <jbenadiva95@gmail.com>
2023-04-24 13:38:55 -0400

    Update API key usage

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import openai
 from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
-openai.api_key = "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"
+openai.api_key = os.environ["OPENAI_API_KEY"]
 
 
 @app.route("/", methods=("GET", "POST"))
